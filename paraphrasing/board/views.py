6f4c3001aaaa0778168aa2f32007cf6820a15bd0
M-Horiuchi <83805270+VinoAsAuksInAMoat@users.noreply.github.com>
2021-09-05 01:15:34 +0900

    Views bert ls (#1)
    * Update views.py
    
    * 入力の不具合を修正．不要なコードを削除．
    
    * 不要なコードを削除．
    
    * UIデザインの変更．
    
    * UIデザインの変更．
    
    * UIデザインの変更．

--- paraphrasing/board/views.py
+++ paraphrasing/board/views.py
@@ -1,8 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-import gensim.downloader
-from gensim.models import Word2Vec
-from .models import CurrentInput
 from run_bert import BERT_LS
 
 msg_saved= ""
@@ -12,48 +8,29 @@ bert = BERT_LS()
 # Create your views here.
 def index(request):
     params= {
-        'msg1':'',
-        'result':'',
+        'input_str':'',
+        'output_str':'',
         'highlight_word':[],
     }
     return render(request, 'board/index.html', params)
 
 def form(request):
-    if "change" in request.POST:
-        msg1 = CurrentInput.objects.all()[0].contents
-        #msg = "The cat perched on the mat."
-        """
-        sample_word = "perched"
-        sample_word2 = "sat"
-        #sample_word2 = paraphrasing(sample_word)
-        msg = msg.replace(sample_word, sample_word2)
-        """
-        msg2 = bert_ls(msg1)
-    else:
-        msg1 = request.POST['msg']
-        new_input = CurrentInput(contents=msg1)
-        new_input.save()
-
+    input_str=request.POST["input_str"]
+    try:
+        output_str = bert_ls(input_str)
+    except Exception as e:
+        output_str=f"err: {e}"
     
-    #result = paraphrasing(msg)
     highlight_words_list=["purched"] # list of words that can be paraphrased
     params= {
-        'msg1':'Your Input: '+msg1,
-        'msg2':'Replaced: '+msg2,
-        #'result':'Synonyms: '+result,
+        'input_str':input_str,
+        'output_str':output_str,
         'highlight_words_list':highlight_words_list,
         }
     return render(request, 'board/index.html', params)
 
-def paraphrasing(msg):
-    #model = Word2Vec.load("C:\\Users\\jun14\\Paraphrasing\\paraphrasing\\board\\word2vec.gensim.model")
-    model = gensim.downloader.load('glove-twitter-25')
-    sentenses = model.most_similar(msg)
-    sentense = sentenses[0][0]
-    return sentense
-
-def bert_ls(msg):
-    output = bert.simplify(msg)
-    return output
+def bert_ls(input_str) -> str:
+    output_str = bert.simplify(input_str)
+    return output_str
 
     
