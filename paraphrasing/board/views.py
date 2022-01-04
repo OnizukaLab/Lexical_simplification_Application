c3030ed07db539ea9cc21c5a78fe58614e88d4e9
M-Horiuchi <83805270+VinoAsAuksInAMoat@users.noreply.github.com>
2022-01-04 20:05:37 +0900

    Finish development of the UI (#5)
    * Finish the interface to process characters one by one like translation systems.
    
    * Add demo(gif)
    
    * Update README

--- paraphrasing/board/views.py
+++ paraphrasing/board/views.py
@@ -1,33 +1,17 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-#from run_bert import BERT_LS
 
-#bert = BERT_LS()
+from run_bert import BERT_LS
+
+bert = BERT_LS()
 
 
 def index(request):
     params= {
         'input_str':'',
         'output_str':'',
-        'highlight_word':[],
+        #'highlight_word':[],
     }
-    return render(request, 'board/index.html', params)
-
-def form(request):
-    input_str=request.POST["input_str"]
-    try:
-        #output_str = bert_ls(input_str)
-        #sample: The cat perched on the mat.
-        output_str = input_str.replace("perched", "sat")
-    except Exception as e:
-        output_str=f"err: {e}"
-    
-    highlight_words_list=["perched"] # list of words that can be paraphrased
-    params= {
-        'input_str':input_str,
-        'output_str':output_str,
-        'highlight_words_list':highlight_words_list,
-        }
     return render(request, 'board/index.html', params)
 
 def Ajax_form(request):
@@ -37,12 +21,9 @@ def Ajax_form(request):
     JsonResponse(params)で動作させる（ページの再読み込みをしない）．
     一文字入力すると選択がtextareaから外れてしまう問題を解消．
     """
-    #input_str = request.GET.get('input_str')
-    input_str=request.POST["input_str"]
+    input_str=request.POST.get("input_str")
     try:
-        #output_str = bert_ls(input_str)
-        #sample: The cat perched on the mat.
-        output_str = input_str.replace("perched", "sat")
+        output_str = bert_ls(input_str)
     except Exception as e:
         output_str=f"err: {e}"
     
@@ -50,11 +31,9 @@ def Ajax_form(request):
     params= {
         'input_str':input_str,
         'output_str':output_str,
-        'highlight_words_list':highlight_words_list,
+        #'highlight_words_list':highlight_words_list,
         }
-    return render(request, 'board/index.html', params)
     return JsonResponse(params)
-    #return params
 
 
 
